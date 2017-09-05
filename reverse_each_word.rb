15cfb8ac6c477a543b624f5797b5ddf33c7f4907
Justin Tooley <jstooley823@gmail.com>
2017-09-05 02:33:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 require 'pry'
 def reverse_each_word (sent)
 
-   sent.split(" ").collect{ |word| word.reverse}.join(" ")
-   #reverse_array = [sent.split(" ").each{ |word| word.reverse}].join(" ")
+   #sent.split(" ").collect{ |word| word.reverse}.join(" ")
+   reverse_array = [sent.split(" ").each{ |word| word.reverse}].join(" ")
 
 
 end
